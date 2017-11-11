2fd06791b8bb7b6ffccad19ec4d25bad3f7cb9c2
Alan <alan.khanaev@gmail.com>
2017-11-11 16:32:50 +0300

    End 8 lesson React-router

--- hello-world/src/Header.js
+++ hello-world/src/Header.js
@@ -5,31 +5,31 @@ class Header extends Component {
     render() {
         console.log('items', this.props.items);
         return (
-            <div>
-                { this.props.items.map((item, index) =>
+            <div>header
+                {/* { this.props.items.map((item, index) =>
                     <a href='{item.link}' key={index}>{item.label}</a>
-                )}
+                )} */}
             </div>
         );
     }
 }
 
-Header.propTypes = {
-    items: PropTypes.array.isRequired,
-    isLoading: PropTypes.bool,
-    submit: PropTypes.func.isRequired,
-    title:PropTypes.string.isRequired,
-    type: PropTypes.oneOf(['news', 'photos']),
-    user: PropTypes.shape({
-        name: PropTypes.string,
-        age: PropTypes.number
-    }),
-    users: PropTypes.arrayOf(
-        PropTypes.shape({
-            name: PropTypes.string,
-            age: PropTypes.number
-        })
-    )
-}
+// Header.propTypes = {
+//     items: PropTypes.array.isRequired,
+//     isLoading: PropTypes.bool,
+//     submit: PropTypes.func.isRequired,
+//     title:PropTypes.string.isRequired,
+//     type: PropTypes.oneOf(['news', 'photos']),
+//     user: PropTypes.shape({
+//         name: PropTypes.string,
+//         age: PropTypes.number
+//     }),
+//     users: PropTypes.arrayOf(
+//         PropTypes.shape({
+//             name: PropTypes.string,
+//             age: PropTypes.number
+//         })
+//     )
+// }
 
 export default Header;
--- hello-world/src/index.js
+++ hello-world/src/index.js
@@ -2,12 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Header from './Header';
 import registerServiceWorker from './registerServiceWorker';
-import {Router, Route, hashHistory} from 'react-router';
+import {  BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 ReactDOM.render(
-<Router history={hashHistory}>
-<Route path="/" component={App}/>
-</Router>
+    <Router>
+        <Switch>
+         <Route exact path='/' component={App}/>
+         <Route path='/header' component={Header}/>
+      </Switch>
+   </ Router >
 , document.getElementById('root'));
 registerServiceWorker();
